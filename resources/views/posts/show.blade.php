130907820de0065b9c1cdff23035b09dc473f603
Dome <arillotta@gmail.com>
2025-02-12 16:00:01 +0100

    created a view for single post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', $post->title)
+
+@section('content')
+    <div class="container">
+        <h1>{{ $post->title }}</h1>
+        <p class="text-muted">By {{ $post->author ?? 'Unknown' }} | {{ $post->created_at->format('d M Y') }}</p>
+        <p>{{ $post->content }}</p>
+
+        <a href="{{ route('posts.index') }}" class="btn btn-secondary">Back to Posts</a>
+        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
+
+        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="d-inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+        </form>
+    </div>
+@endsection
